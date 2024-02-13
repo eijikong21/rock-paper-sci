800f65d626af87ab11fb8e19ebb55c75ed0c6ba1
eijikong21 <eijinakayama21@gmail.com>
2024-02-13 13:46:08 +0800

    Basic Rock Paper Scissors Game

--- myscripts.js
+++ myscripts.js
@@ -3,23 +3,41 @@ let playChoice;
 function getComputerChoice(){
    cpuChoice = Math.floor(Math.random() * 3)+1;
     switch(cpuChoice){
-        case 1: cpuChoice="Rock"; break;
-        case 2:cpuChoice="Paper"; break;
-        case 3:cpuChoice="Scissors"; break;
+        case 1: cpuChoice="rock"; break;
+        case 2:cpuChoice="paper"; break;
+        case 3:cpuChoice="scissors"; break;
     }
-
+    return cpuChoice;
 }
-function getPlayerChoice(){
+function getPlayerChoice(playChoice){
     playChoice = prompt("Enter your decision Rock, Paper, Scissors: ","");
-    battleStart();
+    return playChoice.toLowerCase();
 }
 
-function battleStart(){
-    if(cpuChoice==playChoice){
-        console.log("Player: "+playChoice+" Cpu: "+cpuChoice+"\nIts a draw!");
+function battleStart(playerSelection, computerSelection){
+    playerSelection = getPlayerChoice(playChoice);
+    if(playerSelection==computerSelection){
+        console.log("Player: "+playerSelection+" Cpu: "+computerSelection+"\nIts a draw!");
     }
+    else if(playerSelection=="rock"&&computerSelection=="paper"){
+        console.log("Player: "+playerSelection+" Cpu: "+computerSelection+"\nYou lose!");
+    }
+    else if(playerSelection=="rock"&&computerSelection=="scissors")
+    console.log("Player: "+playerSelection+" Cpu: "+computerSelection+"\nYou Win!");
+    else if(playerSelection=="paper"&&computerSelection=="rock"){
+        console.log("Player: "+playerSelection+" Cpu: "+computerSelection+"\nYou Win!");
+    }
+    else if(playerSelection=="paper"&&computerSelection=="scissors")
+    console.log("Player: "+playerSelection+" Cpu: "+computerSelection+"\nYou Lose!");
+    else if(playerSelection=="scissors"&&computerSelection=="rock"){
+        console.log("Player: "+playerSelection+" Cpu: "+computerSelection+"\nYou Lose!");
+    }
+    else if(playerSelection=="scissors"&&computerSelection=="paper")
+    console.log("Player: "+playerSelection+" Cpu: "+computerSelection+"\nYou Win!");
 }
-getComputerChoice();
-getPlayerChoice();
-
-console.log(cpuChoice)
+function playGame(){
+    
+}
+let playerSelection;
+let computerSelection = getComputerChoice(cpuChoice);
+console.log(battleStart(playerSelection, computerSelection));
